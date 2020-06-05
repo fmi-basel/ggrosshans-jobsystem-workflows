9229dc6fd6e49d8fc98f4046fbbb77751c9d9569
Markus Rempfler <markus.rempfler@fmi.ch>
2020-06-05 15:34:04 +0200

    Fix scikit-image dependency to <0.17

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'luigi',
         'pytest',
-        'scikit-image',
+        'scikit-image>=0.16,<0.17',
         'numpy',
         'tqdm',
         'faim-luigi',
