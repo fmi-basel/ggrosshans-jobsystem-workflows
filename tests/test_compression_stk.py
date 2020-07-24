4be81eb0a7c5d22fefca4732b5d80c4652f18867
Markus Rempfler <markus.rempfler@fmi.ch>
2020-07-24 15:57:36 +0200

    Add test for .nd file backup

--- tests/test_compression_stk.py
+++ tests/test_compression_stk.py
@@ -1,5 +1,7 @@
 import os
 from glob import glob
+import filecmp
+
 import luigi
 import numpy as np
 import pytest
@@ -57,3 +59,39 @@ def test_compression_task(tmpdir, workflow):
         assert ref.ndim == 3
         compr = compr.load()
         assert np.all(ref == compr)
+
+
+@pytest.mark.parametrize(
+    'workflow', [StkToCompressedTifTask, StkToTifImageCompressionWorkflow])
+def test_ndfile_backup(tmpdir, workflow):
+    '''
+    '''
+    input_folder = TEST_DATA['img']
+    test_dir = tmpdir
+
+    def _create_fake_file(folder, fname):
+        path = os.path.join(folder, fname)
+        with open(path, 'w') as fout:
+            fout.write('something')
+        return path
+
+    original = _create_fake_file(input_folder, 'xy.nd')
+    expected = os.path.join(str(test_dir), 'xy.nd.backup')
+
+    result = luigi.build([
+        workflow(output_folder=str(test_dir),
+                 input_folder=input_folder,
+                 file_pattern='*.stk')
+    ],
+                         local_scheduler=True,
+                         detailed_summary=True)
+
+    if result.status not in [
+            luigi.execution_summary.LuigiStatusCode.SUCCESS,
+            luigi.execution_summary.LuigiStatusCode.SUCCESS_WITH_RETRY
+    ]:
+        raise RuntimeError(
+            'Luigi failed to run the workflow! Exit code: {}'.format(result))
+
+    assert os.path.exists(expected)
+    assert filecmp.cmp(original, expected, shallow=False)
