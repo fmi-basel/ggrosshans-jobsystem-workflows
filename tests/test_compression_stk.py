c2432f883bacd323de0b7f1fa85c6db665fb1876
Markus Rempfler <markus.rempfler@fmi.ch>
2020-10-01 10:19:27 +0200

    Add test to check for correct pixel spacing

--- tests/test_compression_stk.py
+++ tests/test_compression_stk.py
@@ -5,6 +5,7 @@ import filecmp
 import luigi
 import numpy as np
 import pytest
+import tifffile
 
 from faim_luigi.targets.image_target import TiffImageTarget
 from ggjw.tasks.compression.compression_simple import StkToCompressedTifTask
@@ -97,3 +98,60 @@ def test_ndfile_backup(tmpdir, workflow):
 
     assert os.path.exists(expected)
     assert filecmp.cmp(original, expected, shallow=False)
+
+
+@pytest.mark.parametrize(
+    'workflow', [StkToCompressedTifTask, StkToTifImageCompressionWorkflow])
+def test_pixel_spacing_meta(tmpdir, workflow):
+    '''check if spacing is correctly propagated.
+    '''
+    input_folder = TEST_DATA['img']
+    test_dir = tmpdir
+
+    reference = [
+        TiffImageTarget(path)
+        for path in sorted(glob(os.path.join(TEST_DATA['img'], '*stk')))
+    ][0]
+    file_pattern = os.path.basename(reference.path)
+
+    result = luigi.build(
+        [
+            workflow(output_folder=str(test_dir),
+                     input_folder=input_folder,
+                     file_pattern=file_pattern)  # only one file is enough.
+        ],
+        local_scheduler=True,
+        detailed_summary=True)
+
+    if result.status not in [
+            luigi.execution_summary.LuigiStatusCode.SUCCESS,
+            luigi.execution_summary.LuigiStatusCode.SUCCESS_WITH_RETRY
+    ]:
+        raise RuntimeError(
+            'Luigi failed to run the workflow! Exit code: {}'.format(result))
+
+    compressed = [
+        TiffImageTarget(path)
+        for path in sorted(glob(os.path.join(str(test_dir), '*tif')))
+    ]
+
+    assert len(compressed) == 1
+
+    def get_resolution(path):
+        with tifffile.TiffFile(path) as fin:
+            if fin.is_stk:
+                return tuple(fin.stk_metadata[ax + 'Calibration']
+                             for ax in 'XY')
+            if fin.is_imagej:
+                return tuple(
+                    denom / num  # inverted to get spacing.
+                    for num, denom in (fin.pages[0].tags[ax +
+                                                         'Resolution'].value
+                                       for ax in 'XY'))
+
+    # compare spacing
+    comp_res = get_resolution(compressed[0].path)
+    ref_res = get_resolution(reference.path)
+
+    assert all(lhs == pytest.approx(rhs, abs=1e-4)
+               for lhs, rhs in zip(comp_res, ref_res))
