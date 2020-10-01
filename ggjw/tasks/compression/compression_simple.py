47ac70f6e3bfba568d60e6241455929de016ef0d
Markus Rempfler <markus.rempfler@fmi.ch>
2020-10-01 10:20:29 +0200

    Add propagation of pixel spacing through compression

--- ggjw/tasks/compression/compression_simple.py
+++ ggjw/tasks/compression/compression_simple.py
@@ -3,10 +3,43 @@ from glob import glob
 import shutil
 
 import luigi
+import tifffile
 
 from faim_luigi.targets.image_target import TiffImageTarget
 
 from .compression_base import BaseCompressionTask
+
+
+def load_stk_with_basic_meta(path):
+    '''
+    '''
+    with tifffile.TiffFile(path) as fin:
+        meta = stk_meta_to_ijtiff_meta(fin.stk_metadata) if fin.is_stk else {}
+        data = fin.asarray()
+        return data, meta
+
+
+def stk_meta_to_ijtiff_meta(stk_metadata):
+    '''extracts pixel spacing and creates the necessary fields
+    to write it to an imagej compatible tiff.
+    '''
+    meta = {
+        'imagej':
+        True,
+        'resolution':
+        tuple(1. / stk_metadata.get(ax + 'Calibration') for ax in 'XY'),
+        'metadata': {
+            'unit': 'um'
+        }
+    }
+
+    try:
+        zdist = stk_metadata['ZDistance'][0]
+        if all(zdist == val for val in stk_metadata['ZDistance']):
+            meta['metadata']['spacing'] = zdist
+    except Exception:
+        pass
+    return meta
 
 
 class StkToCompressedTifTask(BaseCompressionTask):
@@ -32,8 +65,11 @@ class StkToCompressedTifTask(BaseCompressionTask):
     def convert(self, input_target, output_target):
         '''writes tif
         '''
-        img = input_target.load()
-        output_target.save(img, compress=self.compression)
+        img, meta = load_stk_with_basic_meta(input_target.path)
+        if not meta:
+            self.log_warning('Could not read pixel spacing for {}'.format(
+                input_target.path))
+        output_target.save(img, compress=self.compression, **meta)
 
     def preconvert(self):
         '''
