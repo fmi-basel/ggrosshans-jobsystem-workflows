ca6e0ac6339b65a59787410f0cbf0753eefedf7a
Markus Rempfler <markus.rempfler@fmi.ch>
2020-11-24 09:23:36 +0100

    Add test with filtered pattern for quantification workflow

--- tests/test_worm_quantification_knime_workflow.py
+++ tests/test_worm_quantification_knime_workflow.py
@@ -16,6 +16,13 @@ TEST_DATA = {
 TEST_DATA_EXISTS = all(os.path.exists(path) for path in TEST_DATA.values())
 
 
+def assert_quant_output_exists(output_folder):
+    assert (output_folder / 'kymograph_stats.csv').exists()
+    assert (output_folder / 'kymograph_stats.table').exists()
+    assert (output_folder / 'knime-workflow.log').exists()
+    assert (output_folder / 'kymographs').exists()
+
+
 # TODO consider adding check for knime availability
 @pytest.mark.skipif(not TEST_DATA_EXISTS,
                     reason='Test data for quantification not available.')
@@ -46,15 +53,48 @@ def test_quant_workflow(tmpdir, image_file_pattern):
             'Luigi failed to run the workflow! Exit code: {}'.format(result))
 
     # check if outputs are created.
-    result_path = test_dir / 'kymograph_stats.csv'
-    assert result_path.exists()
-
-    assert (test_dir / 'kymograph_stats.table').exists()
-    assert (test_dir / 'knime-workflow.log').exists()
-    assert (test_dir / 'kymographs').exists()
+    assert_quant_output_exists(test_dir)
 
     # check the content of the table.
-    result_data = pandas.read_csv(result_path)
+    result_data = pandas.read_csv(test_dir / 'kymograph_stats.csv')
     assert len(result_data) == 3  # expected number of timepoints
     assert result_data.Intensity_BGsub.between(2, 5).all()
     assert result_data.Frame.between(17, 19).all()
+
+
+@pytest.mark.skipif(not TEST_DATA_EXISTS,
+                    reason='Test data for quantification not available.')
+@pytest.mark.parametrize('image_file_pattern', [
+    '*t17*.stk',
+])
+def test_quant_workflow_filtered(tmpdir, image_file_pattern):
+    '''test the workflow for worm segmentation from BrightField image stacks
+    on where the given pattern matches only one file.
+
+    '''
+    test_dir = tmpdir / 'quantification-knime'
+    test_dir.mkdir()
+
+    result = luigi.build([
+        WormQuantificationWorkflow(output_folder=str(test_dir),
+                                   image_folder=TEST_DATA['img'],
+                                   segm_folder=TEST_DATA['segm'],
+                                   image_file_pattern=image_file_pattern,
+                                   threshold=127.0)
+    ],
+                         local_scheduler=True,
+                         detailed_summary=True)
+
+    if result.status not in [
+            luigi.execution_summary.LuigiStatusCode.SUCCESS,
+            luigi.execution_summary.LuigiStatusCode.SUCCESS_WITH_RETRY
+    ]:
+        raise RuntimeError(
+            'Luigi failed to run the workflow! Exit code: {}'.format(result))
+
+    # check if outputs are created.
+    assert_quant_output_exists(test_dir)
+
+    # check the content of the table.
+    result_data = pandas.read_csv(test_dir / 'kymograph_stats.csv')
+    assert len(result_data) == 1  # expected number of timepoints
