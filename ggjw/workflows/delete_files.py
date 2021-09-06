28b57299d9b8c4bd648633a6fcd3d1c4eac5a553
maritvanderdoes <maritvddoes@gmail.com>
2021-09-06 16:07:17 +0200

    Update delete_files.py

--- ggjw/workflows/delete_files.py
+++ ggjw/workflows/delete_files.py
@@ -7,8 +7,9 @@ from ggjw.tasks.data_management.delete_files import DeleteFilesTask
 
 
 class DeleteFilesWorkflow(luigi.WrapperTask, JobSystemWorkflow):
-    '''executes the worm quantification workflow on a given set of images
-    and their corresponding segmentations.
+    '''executes the delete files workflow that delete the files based on
+    a given "goodworms.csv" csv files that indicates when the worm hatch 
+    and excape.
 
     '''
     image_folder = luigi.Parameter()
@@ -38,7 +39,7 @@ class DeleteFilesWorkflow(luigi.WrapperTask, JobSystemWorkflow):
     resources = {'gpu': 0}
 
     def requires(self):
-        '''launch the actual quantification task.
+        '''launch the actual deleting file task.
         '''
         yield DeleteFilesTask(
             image_folder=self.image_folder,
