26d47708caf6c757c7f0e390257491318e234612
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2021-09-09 11:11:32 +0200

    Update documentation.

--- ggjw/workflows/stack_files.py
+++ ggjw/workflows/stack_files.py
@@ -1,44 +1,46 @@
-'''Provides quantification workflows for image-based assay.
-
-'''
 import luigi
 from ggjw.workflows.base import JobSystemWorkflow
 from ggjw.tasks.data_management.stack_files import StackFilesTask
 
 
 class StackFilesWorkflow(luigi.WrapperTask, JobSystemWorkflow):
-    '''executes the stack files workflow that stacks the images of a 
+    """
+    Executes the stack files workflow that stacks the images of a
     certain channel in z and t.
+    """
 
-    '''
     image_folder = luigi.Parameter()
-    '''folder containing the images to be processed.
-
-    '''
+    """
+    Folder containing the images to be processed.
+    """
 
     image_file_pattern = luigi.Parameter()
-    '''fname pattern matching images of the channel that should
+    """
+    Filename pattern matching images of the channel that should
     be quantified. E.g. "*w1*" for all images with w1 in the filename.
-
-    '''
+    """
 
     {"default": "st", "choises": ["st", "ts"]}
     data_format = luigi.ChoiceParameter(choices=["st", "ts"], default="st")
-    '''order of time and position in files: 's_t_' or 't_s_'.
+    """
+    Visiview automatically writes the data out with 's_' (referring to the stage position)
+    and 't_' (referring to the time point) in the file-name. However, sometimes the order of 's_'
+    and 't_' is inverted. Here we provide information on this order.
+    """
 
-    '''
     output_folder = luigi.Parameter()
-    '''output folder into which the quantification results will be
+    """
+    Output folder into which the quantification results will be
     written.
-
-    '''
+    """
 
     task_namespace = 'ggrosshans'
     resources = {'gpu': 0}
 
     def requires(self):
-        '''launch the actual stacking task.
-        '''
+        """
+        Launch the actual stacking task.
+        """
         yield StackFilesTask(
 
             image_folder=self.image_folder,
