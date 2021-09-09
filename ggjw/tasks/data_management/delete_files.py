1cc16870ccb155e0f7c73786d25bff93f0eb724f
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2021-09-09 11:59:07 +0200

    Refactor code.

--- ggjw/tasks/data_management/delete_files.py
+++ ggjw/tasks/data_management/delete_files.py
@@ -1,4 +1,3 @@
-# %%
 import pandas as pd
 import numpy as np
 import glob
@@ -11,12 +10,18 @@ from ggjw.tasks.lgrunner.stop import StoppableTaskMixin
 
 
 class DeleteFilesTask(luigi.Task, LGRunnerLoggingMixin, StoppableTaskMixin):
+    """
+    Executes the delete files workflow that deletes the images of worms which
+    have not hatched or have escaped. The deletion is based on a provided
+    'goodworms.csv' files, which indicates for each worm when it hatches and
+    when it escapes.
+    """
 
     # inputs from luigi
     image_folder = luigi.Parameter()
     data_format = luigi.ChoiceParameter(choices=["st", "ts"], default="st")
     csv_document = luigi.Parameter()
-    image_file_pattern = luigi.Parameter()
+    image_file_pattern = luigi.Parameter(default="*.tif")
 
     def run(self):
 
@@ -31,27 +36,90 @@ class DeleteFilesTask(luigi.Task, LGRunnerLoggingMixin, StoppableTaskMixin):
         # get annotation file
         if os.path.exists(self.csv_document):
             CSV_annotation = pd.read_csv(self.csv_document)
-
         else:
             raise FileNotFoundError(
                 'CSV file in path {} is not there'
-                .format(self.csv_document))
+                    .format(self.csv_document))
 
+        self.validate(CSV_annotation)
+
+        filelist = self.extract_position_and_frame(img_list)
+
+        filelist = self.merge(CSV_annotation, filelist)
+
+        files_to_delete = self.extract_files_to_delete(filelist)
+
+        # info's
+        self.log_info(
+            "{} Nr of images are going to be delted".format(
+                np.size(files_to_delete, 0)))
+
+        # delete files in folder
+        for i in range(np.size(files_to_delete, 0)):
+            os.remove(files_to_delete["File"].iloc[i])
+
+        self.log_info("Files are deleted")
+
+    def validate(self, CSV_annotation):
+        """
+        Check csv headers.
+        """
         if "Position" not in CSV_annotation:
             raise FileNotFoundError(
                 'CSV document doesnt contain header Position')
-
         if "Quality" not in CSV_annotation:
             raise FileNotFoundError(
                 'CSV document doesnt contain header Quality')
-
         if "Hatch" not in CSV_annotation:
             raise FileNotFoundError('CSV document doesnt contain header Hatch')
         if "Escape" not in CSV_annotation:
             raise FileNotFoundError(
                 'CSV document doesnt contain header Escape')
 
-        # determine position and frame of each image
+    def extract_files_to_delete(self, filelist):
+        """
+        Reduces the filelist to only the files that need to be deleted.
+
+        A file needs to be deleted if it falls into any of the following categories:
+        * Quality == 0
+        * Quality == 1 and Frame < Hatch
+        * Quality == 1 and Frame > Escape
+        """
+        files_to_delete1 = filelist[filelist['Quality'] == 0]
+
+        files_to_delete2 = filelist[filelist['Quality'] == 1]
+        files_to_delete2 = files_to_delete2[(files_to_delete2["Frame"] < files_to_delete2["Hatch"]) | (
+                files_to_delete2["Frame"] > files_to_delete2[
+            "Escape"])]
+
+        files_to_delete = pd.concat([files_to_delete1, files_to_delete2])
+        files_to_delete.to_csv(os.path.join(
+            self.image_folder, "deleted_files.csv"))
+
+        return files_to_delete
+
+    def merge(self, CSV_annotation, filelist):
+        """
+        Merges annotation file and image folder list.
+        """
+        filelist = filelist.merge(
+            CSV_annotation, on=['Position'], how='inner')  # merges two datasets
+        filelist.to_csv(os.path.join(self.image_folder, "original_files.csv"))
+        return filelist
+
+    def extract_position_and_frame(self, img_list):
+        """
+        Determine position and frame/time of each image in `img_list`.
+
+        Parameters:
+        -----------
+        img_list: List
+            A list of all images.
+
+        Returns:
+        --------
+            Returns img_list with additional columns for position and time.
+        """
         AllData = []
         for image in img_list:
             if self.data_format == 'st':
@@ -66,41 +134,8 @@ class DeleteFilesTask(luigi.Task, LGRunnerLoggingMixin, StoppableTaskMixin):
             Data = {"File": image, 'Position': int(
                 pictureinfo[s_info]), 'Frame': int(pictureinfo[t_info])}
             AllData.append(Data)
-
         filelist = pd.DataFrame(data=AllData)
-
-        # merges annotation file and image folder list
-        filelist = filelist.merge(
-            CSV_annotation, on=['Position'], how='inner')  # merges two datasets
-
-        filelist.to_csv(os.path.join(self.image_folder, "original_files.csv"))
-
-        # reduces the filelist to only the files that need to be deleted
-        # selects only worms where quality is 0
-        files_to_delete1 = filelist[filelist['Quality'] == 0]
-
-        # select quality is 1, but where timepoints are out of Hatch/Escape
-        files_to_delete2 = filelist[filelist['Quality'] == 1]
-        files_to_delete2 = files_to_delete2[(files_to_delete2["Frame"] < files_to_delete2["Hatch"]) | (
-            files_to_delete2["Frame"] > files_to_delete2["Escape"])]  # selects only worms where frame is smaller then hatch
-
-        # merges the files to delete
-        files_to_delete = pd.concat([files_to_delete1, files_to_delete2])
-
-        files_to_delete.to_csv(os.path.join(
-            self.image_folder, "deleted_files.csv"))
-
-        # info's
-        self.log_info(
-            "{} Nr of images are going to be delted".format(
-                np.size(files_to_delete, 0)))
-
-        # delete files in folder
-        for i in range(np.size(files_to_delete, 0)):
-            # print(files_to_delete["File"].iloc[i])
-            os.remove(files_to_delete["File"].iloc[i])
-
-        self.log_info("Files are deleted")
+        return filelist
 
     def output(self):
         return luigi.LocalTarget(
